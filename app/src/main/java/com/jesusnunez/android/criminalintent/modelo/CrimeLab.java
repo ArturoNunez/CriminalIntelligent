78c2c3d23ff2b783836c8379da3155be36b7aaae
Jesus Arturo Nunez Perez <E17470003@itcampeche.edu.mx>
2019-12-05 05:36:17 -0600

    Agrega persistencia al proyecto utilizando SQLite

--- app/src/main/java/com/jesusnunez/android/criminalintent/modelo/CrimeLab.java
+++ app/src/main/java/com/jesusnunez/android/criminalintent/modelo/CrimeLab.java
@@ -1,14 +1,23 @@
 package com.jesusnunez.android.criminalintent.modelo;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.jesusnunez.android.criminalintent.database.CrimeBaseHelper;
+import com.jesusnunez.android.criminalintent.database.CrimeCursorWrapper;
+import com.jesusnunez.android.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+    //private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -18,25 +27,84 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime # " + (i+1));
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
+        //mCrimes = new ArrayList<>();
+        //for (int i = 0; i < 100; i++) {
+        //  Crime crime = new Crime();
+        //  crime.setTitle("Crime # " + (i+1));
+        // crime.setSolved(i % 2 == 0);
+        // mCrimes.add(crime);
+        // }
+        mContext = context;
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
+    }
+
+        public void addCrime(Crime c) {
+            ContentValues values = getContentValues(c);
+            mDatabase.insert(CrimeTable.NAME, null, values);
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        //return mCrimes;
+        //return new ArrayList<>();
+        List<Crime> crimes = new ArrayList<>();
+        CrimeCursorWrapper cursor = queryCrimes(null, null);
+        try {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                crimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+        return crimes;
     }
 
-    public  Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
+    public Crime getCrime(UUID id) {
+    //return null;
+        CrimeCursorWrapper cursor = queryCrimes(
+                CrimeTable.Cols.UUID + " = ?",
+                new String[] {id.toString()}
+        );
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
             }
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
         }
-        return null;
     }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+        return values;
+    }
+
+    public void onPause() {
+        super.onPause();
+        CrimeLab.get(getActivity()).updateCrime(mCrime);
+    }
+
+    //private Cursor queryCrimes(String whereClause, String[] whereArgs) {
+    private CrimeCursorWrapper queryCrimes(String whereClause, String[] whereArgs){
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null, //Columnas a recuperar; null recupera todas las columnas
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return new CrimeCursorWrapper(cursor);
+    }
+
 }
+
